f667e8e46a908962827f711275afeeda365c353c
Hiroto Kawabata <13412823+hkawabata@users.noreply.github.com>
2023-02-18 17:23:04 +0900

    2023/02/18 17:23 update

---
+++ bin/tmp.sh
@@ -0,0 +1,3 @@
+echo $0
+echo `dirname $0`
+echo `basename $0`
--- bin/update.sh
+++ bin/update.sh
@@ -1,3 +1,7 @@
+DIR=$(dirname $0)
+
+echo ${DIR}/path_tree_generator.py > ${DIR}/../docs/_includes/pagetree.xml
+
 git add --all && \
   git commit -m"$(date +%Y/%m/%d\ %H:%M) update" && \
   git push
